99a554047344a85872c2bada22c5b7f098cc6470
wuhualin <haishuizhijia@163.com>
2017-11-27 22:25:01 +0800

    Initial Commit

---
+++ HZRequestFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HZRequestFramework
+//
+//  Created by whl0302 on 2017/11/27.
+//  Copyright © 2017年 com.huazai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
